4ac72308d093ef2ae27cfefef2aa5c995eb261af
Rufus Pollock <rufus.pollock@okfn.org>
2013-05-12 15:24:36 +0100

    [all][l]: heavily improve and refactor.
    * Switch from listify to simple JS
    * Redo templating away from tables
    * Gravatar support for owners
    * Show/hide more info

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
 jQuery(function($) {
   var url = 'https://docs.google.com/a/okfn.org/spreadsheet/ccc?key=0AvdkMlz2NopEdElqWTBJS0Q1Q083VlI3YUFLTl9OY0E#gid=0';
+  var $el = $('.load-status');
   var tmpl = $('#our-template').html();
-  var listify = new Listify.App({
-    el: '.load-status',
-    url: url,
-    template: tmpl
-  }, function(err) {
-    listify.queryResults = listify.queryResults.slice(1);
-    listify.render();
-  });
+  recline.Backend.GDocs.fetch({url: url})
+    .done(function(results) {
+      // take off first row as it is instructions
+      results.records = results.records.slice(1);
+      results.records = _.map(results.records, function(record) {
+        record.owners = parseOwners(record.owners);
+        record.noOwner = (record.owners.length === 0)
+        return record
+      });
+      var out = Mustache.render(tmpl, results);
+      $el.html(out);
+    });
   $('.source-url').attr('href', url);
+  $('.js-more-info').live('click', function(e) {
+    e.preventDefault();
+    $(e.target) .closest('.record').find('.more-info').toggle();
+  });
 });
+
+function strip(s) {
+  return s.replace(/^ */, '').replace(/ *$/, '');
+}
+
+function parseOwners(owners) {
+  owners = strip(owners);
+  if (owners == '') {
+    return [];
+  }
+  var out = _.map(owners.split(','), function(owner) {
+    var parts = strip(owner).match(/^([^<]*)(<(.*)>)?/);
+    newowner = {
+      name: parts[1],
+      email: parts[3] || ''
+    }
+    if (newowner.email) {
+      newowner.gravatar = 'http://www.gravatar.com/avatar/' + md5(newowner.email.toLowerCase()) + '?s=40&d=retro';
+    }
+    return newowner;
+  });
+  return out;
+}
